bf640a4996f6b1203650be92ec1c315f1139cd2d
evo <84800113+evo0616lution@users.noreply.github.com>
2021-11-23 08:42:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,29 @@ import discord
 from discord.ext import commands
 from colorama import Fore, init
 init()
+client = commands.Bot(
+  command_prefix=':',
+  self_bot=True
+)
+client.remove_command('help')
+def logo():
+  print(Fore.YELLOW + """
+
+    ___                _____ __        __            
+   /   |  ____  __  __/ ___// /_____ _/ /___  _______
+  / /| | / __ \/ / / /\__ \/ __/ __ `/ __/ / / / ___/
+ / ___ |/ / / / /_/ /___/ / /_/ /_/ / /_/ /_/ (__  ) 
+/_/  |_/_/ /_/\__, //____/\__/\__,_/\__/\__,_/____/  
+             /____/                                  
+
+""")
+  print(Fore.RESET + "Made by https://github.com/evo0616lution")
+
+  
+  
+  
+logo()
+with open('token.yaml') as info:
+    TOKEN = next(yaml.load_all(info, Loader=yaml.FullLoader))
+with open('text.yaml') as info:
+    TEXT = next(yaml.load_all(info, Loader=yaml.FullLoader)) 
